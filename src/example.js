03b8e0cfc4fecc22f8fe51f8eeda9498c75f8a66
Dinan <dinandilmith58@gmail.com>
2026-03-14 12:43:36 +0530

    fix: ensure whitespace is trimmed when comparing string lengths

--- src/example.js
+++ src/example.js
@@ -1,5 +1,5 @@
 export function longestString(str1, str2) {
-  if (str2.length > str1.length) return str2
+  if (str2.trim().length > str1.trim().length) return str2;
 
-  return str1
+  return str1;
 }
