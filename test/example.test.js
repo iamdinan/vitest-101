84f0a008f95b773378abf50163645d6b6b029afe
Dinan <dinandilmith58@gmail.com>
2026-03-14 12:43:48 +0530

    test: update longestString tests for accuracy and whitespace handling

--- test/example.test.js
+++ test/example.test.js
@@ -1,10 +1,27 @@
 import { describe, it, expect } from "vitest";
 import { longestString } from "../src/example";
 
+// can import "test" from vitest and use it instead of "it" — both are identical
+
 describe("example.longest string", () => {
   it("should return the longest string", () => {
-    const longest = longestString("superman", "spiderman");
+    const longest = longestString("superman", "batman");
+    //longestString function assigned to longest variable and called variable in expect function
+    expect(longest).toBe("superman");
+  });
 
-    expect(longest).toBe("spiderman");
+  it("should return first string when both strings are same length", () => {
+    //longest string function directly called inside expect function. both approaches are same
+    expect(longestString("antman", "batman")).toBe("antman");
+  });
+
+  it("should handle empty strings", () => {
+    expect(longestString(" ", "batman")).toBe("batman");
+    expect(longestString("superman", " ")).toBe("superman");
+    expect(longestString(" ", " ")).toBe(" ");
+  });
+
+  it("ignore whitespace", () => {
+    expect(longestString("superman", "  batman  ")).toBe("superman");
   });
 });
